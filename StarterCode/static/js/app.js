8e044f9202a267077568f3b2943bd73ceb81f966
tamara washington <tamey.washington@gmail.com>
2021-03-10 20:43:10 -0600

    Initial Function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,36 @@
 // Use d3 to read in samples.json
 
-let dataPromise = d3.json("./samples.json");
+// let dataPromise = d3.json("./samples.json");
 
-dataPromise.then(processData);
-
-function processData(data){
-    console.log(data);
-}
-
-
-// d3.json("data/samples.json").then((data) => {
+// function processData(data){
 //     console.log(data);
-// };
+// }
+
+// dataPromise.then(processData);
+
+// Create function to get info needed
+function getInfo() {
+    //Read in JSON
+    d3.json("./samples.json").then ((data) =>{
+        console.log(data)
+    //collect the out ids
+    // var otuId = data.samples[0].otu_ids;
+    // console.log(otuId)
+    });
+
+
+
+//Get a handle on the dropdown
+// data.map((
+
+    
+// ))  
+// let dropDown = d3.select('#selDataset');
+
+
+
+
+// //Get top out_ids
+// function getId(data) {
+//     return data.samples[0].out_ids
+// }
